172541d7a2c70e31d14616cd89282aef8aa83f94
51985 <51985@LAPTOP-PMKPMQER>
2020-09-17 08:41:51 -0500

    codigo de entidad

--- src/main/java/pe/edu/upeu/SV_KIRBY/entity/Rol.java
+++ src/main/java/pe/edu/upeu/SV_KIRBY/entity/Rol.java
@@ -1,5 +1,26 @@
 package pe.edu.upeu.SV_KIRBY.entity;
 
 public class Rol {
-
+private int idrol;
+private String nombre;
+public Rol() {
+	
+}
+public Rol (int idrol, String nombre) {
+	super();
+	this.idrol=idrol;
+	this.nombre=nombre;
+}
+public int getIdrol() {
+	return idrol;
+}
+public void setIdrol (int idrol) {
+	this.idrol=idrol;
+}
+public String getNombre() {
+	return nombre;
+}
+public void setNombre(String nombre) {
+	this.nombre=nombre;
+}
 }
