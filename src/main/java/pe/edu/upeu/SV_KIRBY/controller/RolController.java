b96df94e189ac5139d90b3f83dff2a4d1179966b
aliso <aliso@192.168.1.15>
2020-09-23 12:01:27 -0500

    controladores

--- src/main/java/pe/edu/upeu/SV_KIRBY/controller/RolController.java
+++ src/main/java/pe/edu/upeu/SV_KIRBY/controller/RolController.java
@@ -3,8 +3,12 @@ import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,4 +30,19 @@ public class RolController {
     public Rol read(@PathVariable int id) {
     	return rolService.read(id);
     }
+    @DeleteMapping("/delete/{id}")
+    public int delete(@PathVariable int id) {
+    	return rolService.delete(id);
+    }
+    @PostMapping("/rol")
+    public int create(@RequestBody Rol rol) {
+    	return rolService.create(rol);
+    }
+    @PutMapping("edit/{id}")
+    public int update(@RequestBody Rol rol, @PathVariable int id) {
+    	Rol r = rolService.read(id);
+    	 r.setNomrol(rol.getNomrol());
+    	return rolService.update(r);
+    }
+    
 }
