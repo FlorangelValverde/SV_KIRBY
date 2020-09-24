46c4cb1813550bd88db8e0ddd1ffae25e769967f
aliso <aliso@192.168.1.15>
2020-09-24 11:04:36 -0500

    producto1

--- src/main/java/pe/edu/upeu/SV_KIRBY/dao/Producto.java
+++ src/main/java/pe/edu/upeu/SV_KIRBY/dao/Producto.java
@@ -5,5 +5,40 @@ public class Producto {
 	private String nomprod;
 	private double precio;
 	private int cantidad;
+	public Producto() {
+	}
+	public Producto(int idproducto, String nomprod, double precio, int cantidad) {
+		super();
+		this.idproducto = idproducto;
+		this.nomprod = nomprod;
+		this.precio = precio;
+		this.cantidad = cantidad;
+	}
+	public int getIdproducto() {
+		return idproducto;
+	}
+	public void setIdproducto(int idproducto) {
+		this.idproducto = idproducto;
+	}
+	public String getNomprod() {
+		return nomprod;
+	}
+	public void setNomprod(String nomprod) {
+		this.nomprod = nomprod;
+	}
+	public double getPrecio() {
+		return precio;
+	}
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+	public int getCantidad() {
+		return cantidad;
+	}
+	public void setCantidad(int cantidad) {
+		this.cantidad = cantidad;
+	}
+	
+	
 
 }
